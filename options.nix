5bd7583cf75bc64871df006335d45e835eddce41
omnia <zzzooozzz836@gmail.com>
2025-05-08 14:25:55 +0545

    transparency :]

--- options.nix
+++ options.nix
@@ -8,5 +8,14 @@
       tabstop = 2;
     };
     autopairs.nvim-autopairs.enable = true;
+    luaConfigPre = "
+      vim.cmd([[
+  augroup TransparentBackground
+  autocmd!
+  autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+  autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
+  augroup END
+]])
+        ";
   };
 }
