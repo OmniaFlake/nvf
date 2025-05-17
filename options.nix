409304b2b99aca2e2a1ee8828df5df1f4d51534f
omnia <zzzooozzz836@gmail.com>
2025-05-17 19:56:01 +0545

    upgabuga

--- options.nix
+++ options.nix
@@ -3,10 +3,6 @@
     globals = {
       mapleader = " ";
     };
-    options = {
-      shiftwidth = 2;
-      tabstop = 2;
-    };
     autopairs.nvim-autopairs.enable = true;
     luaConfigPre = "
       vim.cmd([[
@@ -17,6 +13,10 @@
   augroup END
 ]])
       vim.cmd([[colorscheme torte]])
+      vim.cmd([[set tabstop=2]])
+      vim.cmd([[set expandtab]])
+      vim.cmd([[set shiftwidth=2]])
+
         ";
   };
 }
